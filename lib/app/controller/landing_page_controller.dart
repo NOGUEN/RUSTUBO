5138933f4437721eebbd27f15c00e55c85d43bb6
NOGUEN <nhg1113@naver.com>
2024-01-08 16:00:33 +0900

    feat: 랜딩 페이지 메인 타이틀 플로우 구현. 화면 너비에 따른 이미지 표시 대응 필요.

--- lib/app/controller/landing_page_controller.dart
+++ lib/app/controller/landing_page_controller.dart
@@ -1,3 +1,14 @@
+import 'package:get/get.dart';
 import 'package:rutsubo/app/core/base/base_controller.dart';
 
-class LandingPageController extends BaseController {}
+class LandingPageController extends BaseController {
+  Rx<double> scrollPosition = 0.0.obs;
+
+  Rx<double> setOpacity() {
+    if (scrollPosition.value * 0.005 + 0.4 >= 0.8) {
+      return 0.8.obs;
+    }
+
+    return (scrollPosition.value * 0.005 + 0.4).obs;
+  }
+}
